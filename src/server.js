51c44d047d7f7431206ab0e7174fa4d463a02227
carloseduardoalvesviana <eduardostrink@gmail.com>
2025-10-01 15:52:04 -0300

    adicionado: rate limite, ban: 5

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const fastify = require("fastify");
+const rateLimit = require("@fastify/rate-limit");
 const emailRoutes = require("./routes/email");
 const customersRoutes = require("./routes/customers");
 const smsRoutes = require("./routes/sms");
@@ -8,6 +9,26 @@ const whatsappRoutes = require("./routes/whatsapp");
 const prisma = require("./database");
 
 const server = fastify({ logger: true });
+
+/**
+ * Adicionando Rate Limiting (Throttle)
+ * - max: máximo de requests no intervalo
+ * - timeWindow: período em ms ou string (ex: '1 minute')
+ * - ban: opcional → número de violações antes de banir
+ */
+server.register(rateLimit, {
+  max: 100, // até 100 requisições
+  timeWindow: "1 minute", // por minuto
+  ban: 5, // opcional → depois de 5 estouros, o IP fica banido
+  keyGenerator: (req) => req.headers["x-real-ip"] || req.ip, // personaliza a chave (IP real do proxy)
+  errorResponseBuilder: (req, context) => {
+    return {
+      code: 429,
+      error: "Too Many Requests",
+      message: `Você excedeu o limite de ${context.max} requisições em ${context.timeWindow}`,
+    };
+  },
+});
 
 server.get("/", (req, reply) => {
   return reply.status(200).send({
@@ -22,8 +43,6 @@ server.register(whatsappRoutes);
 
 server.post("/webhook-received", async (req, res) => {
   const { phone, instanceId } = req.body;
-
-  console.log(req.body);
 
   const record = await prisma.whatsappNotifications.findFirst({
     where: {
