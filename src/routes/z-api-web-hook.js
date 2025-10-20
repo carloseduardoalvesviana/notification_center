e015f6fa571fa6d98a0cbbcad87baab7238792e8
carloseduardoalvesviana <eduardostrink@gmail.com>
2025-10-20 09:52:23 -0300

    corrigido: zApiWebHook

--- src/routes/z-api-web-hook.js
+++ src/routes/z-api-web-hook.js
@@ -5,6 +5,14 @@ async function zApiWebHook(server) {
   server.post("/webhook-received", async (req, res) => {
     const { phone, instanceId } = req.body;
 
+    console.log(req.body)
+
+    const client = await prisma.whatsappOptionsForCustomers.findFirst({
+      where: {
+        zapi_client_instance: instanceId
+      }
+    })
+
     const record = await prisma.whatsappNotifications.findFirst({
       where: {
         number: `+${phone}`,
@@ -26,14 +34,22 @@ async function zApiWebHook(server) {
       });
     }
 
+    let dataForMobilize = {
+      ...client,
+      ...req.body
+    }
+
+    console.log(dataForMobilize)
+
     try {
       await fetch(env.URL_NOTIFICATION, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(req.body),
+        body: JSON.stringify(dataForMobilize),
       });
+      console.log("Enviando retorno para acessomobilize")
     } catch (error) {
       console.error("Erro ao enviar para central-de-notificacoes:", error);
     }
